c9da38b309ccd43ec48c2f15916954f1d396f0a4
(TAT)chaN <web@lanevok.com>
2012-02-28 22:19:07 +0900

    SOT

--- 0000-1.c
+++
@@ -1,1 +0,0 @@
-main(a,b){for(;a<10;a++){for(b=1;b<10;){printf("%dx%d=%d\n",a,b++,a*b);}}exit(0);}
--- 0048-11.c
+++
@@ -1,1 +0,0 @@
-main(float w){for(;~scanf("%f",&w);)puts(w>48?w>51?w>54?w>57?w>60?w>64?w>69?w>75?w>81?w>91?"heavy":"light heavy":"middle":"light middle":"welter":"light welter":"light":"feather":"bantam":"fly":"light fly");}
