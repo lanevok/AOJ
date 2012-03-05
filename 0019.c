ef34415b5a2a9f484343b303ac13054debd3d695
(TAT)chaN <web@lanevok.com>
2012-03-05 14:33:01 +0900

    SC

--- 0019.c
+++ 0019.c
@@ -1,8 +1,1 @@
-main(n){
-  unsigned long long int r=1;
-  scanf("%d",&n);
-  while(n)
-    r=r*n--;
-  printf("%lld\n",r);
-  exit(0);
-}
+main(n){unsigned long long int r=1;scanf("%d",&n);while(n)r=r*n--;printf("%lld\n",r);exit(0);}
