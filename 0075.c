2f21c0921d77727d4824b8692411d89795ad5d4c
(TAT)chaN <web@lanevok.com>
2012-03-05 14:22:56 +0900

    SC

--- 0075.c
+++ 0075.c
@@ -1,9 +1,1 @@
-#include<stdio.h>
-int main(a){
-  double t,s;
-  while(scanf("%d,%lf,%lf",&a,&t,&s)!=EOF){    
-    if(t/(s*s)>=25.0)
-      printf("%d\n",a);
-  }
-  return 0;
-}
+main(a){float t,s;for(;~scanf("%d,%f,%f",&a,&t,&s);){if(t/(s*s)>=25)printf("%d\n",a);}}
