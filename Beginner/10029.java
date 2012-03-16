99648a8a3742b42ef386d27e542b5eebcdf0d355
(TAT)chaN <web@lanevok.com>
2012-03-16 21:52:58 +0900

    AC

---
+++ Beginner/10029.java
@@ -0,0 +1,19 @@
+﻿import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+	Scanner stdIn = new Scanner(System.in);
+	int n = stdIn.nextInt();
+	int a[] = new int [n];
+	
+	for(int i=0;i<n;i++)
+	    a[i] = stdIn.nextInt();
+	
+	Arrays.sort(a);
+	
+	for(int i=0;i<n-1;i++)
+	    System.out.print(a[i]+" ");
+	System.out.println(a[n-1]);
+    }
+}
