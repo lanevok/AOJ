b9c3bb34689c47f1d09e5f712f7c9395814eead5
(TAT)chaN <web@lanevok.com>
2012-03-16 22:15:40 +0900

    AC

---
+++ Beginner/10031.java
@@ -0,0 +1,25 @@
+﻿import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args){
+	Scanner stdIn = new Scanner(System.in);
+	
+	int n = stdIn.nextInt();
+	TreeSet s = new TreeSet();
+	
+	for(int i=0;i<n;i++) {
+	    s.add(stdIn.nextInt());
+	}
+	
+	int q = stdIn.nextInt();
+	int cnt = 0;
+	
+	for(int i=0;i<q;i++){
+	    if(s.contains(stdIn.nextInt()))
+		cnt++;
+	}
+	System.out.println(cnt);
+    }
+}
