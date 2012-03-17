4f6ddfec05b767f12592e05fed688915b54a4c89
(TAT)chaN <web@lanevok.com>
2012-03-17 10:33:56 +0900

    RE

--- Beginner/10020.c
+++ Beginner/10020.c
@@ -4,7 +4,6 @@
 
 int main(){
   char b[10000];
-  char a[]="abcdefghijklmnopqrstuvwxyz";
   int c[26]={};
   int n,i=0;
   
@@ -17,14 +16,12 @@ int main(){
   n=strlen(b);
 
   for(i=0;i<n;i++){
-    if(isupper(b[i]))
-      b[i]=tolower(b[i]);
-    if(islower(b[i]))
-      c[b[i]-'a']++;
+    b[i]=tolower(b[i]);
+    c[b[i]-'a']++;
   }
 
   for(i=0;i<26;i++)
-    printf("%c : %d\n",a[i],c[i]);
+    printf("%c : %d\n",'a'+i,c[i]);
 
   return 0;
 }
