4867c1318f84541e3d78ab14652ff7e1991ccbcf
(TAT)chaN <web@lanevok.com>
2012-03-18 16:07:04 +0900

    AC

---
+++ 0009.java
@@ -0,0 +1,46 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+    static boolean[] primes;
+    
+    static void primeSet(final int MAX){
+	primes = new boolean[MAX+1];
+	
+	primes[2] = true;
+	for(int i=3;i<=MAX;i+=2){
+	    primes[i] = true;
+	}
+	int rt = (int) Math.sqrt(MAX);
+	for(int i=3;i<=rt;i+=2){
+	    if(primes[i]){
+		for(int j=i*2;j<=MAX;j+=i){
+		    primes[j] = false;
+		}
+	    }
+	}
+    }
+    
+    public static void main(String[] args) {
+	Scanner stdIn = new Scanner(System.in);
+	
+	primeSet(999999);
+	
+	while(stdIn.hasNext()){
+	    int n = stdIn.nextInt();
+	    int cnt = 0;
+	    
+	    if(n<2){
+		System.out.println("0");
+		continue;
+	    }
+	    
+	    for(int i=0;i<=n;i++){
+		if(primes[i]){
+		    cnt++;
+		}
+	    }
+	    System.out.println(cnt);
+	}	
+    }
+}
