89abb2d2698c5d349b2aea62b6a16a04a7d2e5c0
(TAT)chaN <web@lanevok.com>
2012-03-20 21:36:03 +0900

    SEM

---
+++ 0020.java
@@ -0,0 +1,10 @@
+﻿import java.util.Scanner;
+
+public class AOJ0020 {
+    
+    public static void main(String[] args) {
+	Scanner stdIn = new Scanner(System.in);
+	String a = stdIn.nextLine();
+	System.out.println(a.toUpperCase());
+    }
+}
---
+++ 0114.java
@@ -0,0 +1,62 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    
+    static long gcd(long a, long b){
+	
+	if(b==0)
+	    return a;
+	else
+	    return gcd(b,a%b);
+    }
+    
+    static int calc(int a, int x, int m){
+	return a*x%m;
+    }
+    
+    public static void main(String[] args) {
+	Scanner stdIn = new Scanner(System.in);
+	
+	while(true){			
+	    int a[] = new int[3];
+	    int m[] = new int[3];
+	    int cnt[] = new int[3];
+	    int x;
+	    int flag=0;
+	    
+	    for(int i=0;i<3;i++){
+		a[i]=stdIn.nextInt();
+		m[i]=stdIn.nextInt();
+		if(a[i]==0&&m[i]==0){
+		    flag++;
+		}
+	    }
+	    
+	    if(flag==3){
+		break;
+	    }
+	    
+	    for(int i=0;i<3;i++){
+		x=1;
+		while(true){
+		    x=calc(a[i],x,m[i]);
+		    cnt[i]++;
+		    if(x==1){
+			break;
+		    }
+		}
+	    }
+	    
+	    long gcd1 = gcd(cnt[0],cnt[1]);
+	    long gcd2 = gcd(cnt[0],cnt[2]);
+	    
+	    long lcm1 = cnt[0]*cnt[1]/gcd1;
+	    long lcm2 = cnt[0]*cnt[2]/gcd2;
+	    
+	    long gcd_ans = gcd(lcm1,lcm2);
+	    long lcm_ans = lcm1*lcm2/gcd_ans;
+	    
+	    System.out.println(lcm_ans);
+	}
+    }
+}
