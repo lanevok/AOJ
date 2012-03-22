2f61450d77b5a11c9e0de820e16afd60513e0a94
(TAT)chaN <web@lanevok.com>
2012-03-22 21:52:18 +0900

    AC

---
+++ 0502.java
@@ -0,0 +1,65 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+	Scanner stdIn = new Scanner(System.in);
+	
+	while(true){
+	    int n = stdIn.nextInt();
+	    if(n==0){
+		return;
+	    }
+	    
+	    int sum = 1;
+	    int[] dice = {1,3,6,4,2,5};
+	    for(int i=0;i<n;i++){
+		String cmd = stdIn.next();
+		if(cmd.compareTo("North")==0){
+		    int tmp = dice[0];
+		    dice[0] = dice[4];
+		    dice[4] = dice[2];
+		    dice[2] = dice[5];
+		    dice[5] = tmp;
+		}
+		else if(cmd.compareTo("East")==0){
+		    int tmp = dice[0];
+		    dice[0] = dice[3];
+		    dice[3] = dice[2];
+		    dice[2] = dice[1];
+		    dice[1] = tmp;
+		}
+		else if(cmd.compareTo("West")==0){
+		    int tmp = dice[0];
+		    dice[0] = dice[1];
+		    dice[1] = dice[2];
+		    dice[2] = dice[3];
+		    dice[3] = tmp;
+		}
+		else if(cmd.compareTo("South")==0){
+		    int tmp = dice[0];
+		    dice[0] = dice[5];
+		    dice[5] = dice[2];
+		    dice[2] = dice[4];
+		    dice[4] = tmp;
+		}
+		else if(cmd.compareTo("Right")==0){
+		    int tmp = dice[4];
+		    dice[4] = dice[1];
+		    dice[1] = dice[5];
+		    dice[5] = dice[3];
+		    dice[3] = tmp;
+		}
+		else if(cmd.compareTo("Left")==0){
+		    int tmp = dice[4];
+		    dice[4] = dice[3];
+		    dice[3] = dice[5];
+		    dice[5] = dice[1];
+		    dice[1] = tmp;
+		}
+		sum+=dice[0];
+	    }
+	    System.out.println(sum);
+	}
+    }
+}
