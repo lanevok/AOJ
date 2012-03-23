84a0f79170f1b0c11e72430e61fb53f0931ffbef
(TAT)chaN <web@lanevok.com>
2012-03-23 09:38:15 +0900

    AC

---
+++ 0521.java
@@ -0,0 +1,41 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+	Scanner stdIn = new Scanner(System.in);
+	
+	while(true){
+	    int money = 1000 - stdIn.nextInt();
+	    if(money==1000){
+		return;
+	    }
+	    
+	    int coin = 0;
+	    if(money>=500){
+		money-=500;
+		coin++;
+	    }
+	    if(money>=100){
+		int tmp = money/100;
+		money-=tmp*100;
+		coin+=tmp;
+	    }
+	    if(money>=50){
+		money-=50;
+		coin++;
+	    }
+	    if(money>=10){
+		int tmp = money/10;
+		money-=tmp*10;
+		coin+=tmp;
+	    }
+	    if(money>=5){
+		money-=5;
+		coin++;
+	    }
+	    coin+=money;
+	    System.out.println(coin);
+	}
+    }
+}
