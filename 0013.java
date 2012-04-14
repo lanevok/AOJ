b6b4e7a9debf50e1267a762e01a8d7d31610761e
(TAT)chaN <web@lanevok.com>
2012-04-14 10:51:41 +0900

    AC

---
+++ 0013.java
@@ -0,0 +1,19 @@
+﻿import java.util.Scanner;
+import java.util.Stack;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        Stack<Integer> b = new Stack<Integer>();
+        while(a.hasNext()){
+            int c = a.nextInt();
+            if(c==0){
+                System.out.println(b.pop());
+            }
+            else{
+                b.push(c);
+            }
+        }
+    }
+}
