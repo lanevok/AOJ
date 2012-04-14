ae2d3d2cf627129e2df5b36758db4dca249d7f73
(TAT)chaN <web@lanevok.com>
2012-04-14 11:26:16 +0900

    AC

---
+++ 0532.java
@@ -0,0 +1,28 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+         
+        int[] a = new int[6];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<6;j++){
+                a[j]=stdIn.nextInt();
+            }
+            int h=0,m=0,s=0;
+            h=a[3]-a[0];
+            m=a[4]-a[1];
+            s=a[5]-a[2];
+            if(s<0){
+                m--;
+                s+=60;
+            }
+            if(m<0){
+                h--;
+                m+=60;
+            }
+            System.out.println(h+" "+m+" "+s);
+        }
+    }
+}
---
+++ 0533.java
@@ -0,0 +1,26 @@
+﻿import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+         
+        int[] w = new int[10];
+        int[] k = new int[10];
+        for(int i=0;i<10;i++){
+            w[i]=stdIn.nextInt();
+        }
+        for(int i=0;i<10;i++){
+            k[i]=stdIn.nextInt();
+        }
+        Arrays.sort(w);
+        Arrays.sort(k);
+        int sum_w=0, sum_k=0;
+        for(int i=7;i<10;i++){
+            sum_w+=w[i];
+            sum_k+=k[i];
+        }
+        System.out.println(sum_w+" "+sum_k);
+    }
+}
