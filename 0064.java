e8dc0a1ccc8c803da2d7e603d7d81cedc83cd186
(TAT)chaN <web@lanevok.com>
2012-04-14 10:45:33 +0900

    AC

---
+++ 0064.java
@@ -0,0 +1,29 @@
+﻿import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        long sum = 0;
+        long tmp = 0;
+        
+        while(stdIn.hasNext()){
+            String a = stdIn.nextLine();
+            char[] b = new char[100];
+            b = a.toCharArray();
+            tmp = 0;
+            for(int i=0;i<b.length;i++){
+                if(b[i]>='0'&&b[i]<='9'){
+                    tmp*=10;
+                    tmp+=b[i]-'0';
+                }
+                else{
+                    sum+=tmp;
+                    tmp=0;
+                }
+            }
+            sum+=tmp;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ 0513.java
@@ -0,0 +1,60 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    static int[] a;
+    static int n;
+     
+    static void riffle(){
+        int[] b = new int[2*n];
+        for(int i=0;i<2*n;i++){
+            b[i]=a[i];
+        }
+        int k=0;
+        for(int i=0;i<2*n;i+=2){
+            a[i]=b[k];
+            a[i+1]=b[n+k];
+            k++;
+        }
+    }
+     
+    static void cut(int j){
+        int[] b = new int[2*n];
+        for(int i=0;i<2*n;i++){
+            b[i]=a[i];
+        }
+        int k=0;
+        int i;
+        for(i=2*n-1;k<j;i--){
+            a[i]=b[j-1-k];
+            k++;
+        }
+        k=2*n-1;
+        for(;i>=0;i--){
+            a[i]=b[k--];
+        }
+    }
+     
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+         
+        n = stdIn.nextInt();
+        a = new int[2*n];
+        for(int i=0;i<2*n;i++){
+            a[i]=i+1;
+        }
+        int m = stdIn.nextInt();
+        for(int i=0;i<m;i++){
+            int k = stdIn.nextInt();
+            if(k==0){
+                riffle();
+            }
+            else{
+                cut(k);
+            }
+        }
+        for(int i=0;i<2*n;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
