5346e7be57f3b2f6f88dac67af8788334c3937f3
(TAT)chaN <web@lanevok.com>
2012-04-29 13:31:35 +0900

    AC

---
+++ 0500.java
@@ -0,0 +1,33 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+ 
+        while(true){
+            int n = stdIn.nextInt();
+            if(n==0){
+                return;
+            }
+            int a = 0, b = 0;
+            for(int i=0;i<n;i++){
+                int ta = stdIn.nextInt();
+                int tb = stdIn.nextInt();
+                if(ta>tb){
+                    a+=ta;
+                    a+=tb;
+                }
+                else if(ta<tb){
+                    b+=ta;
+                    b+=tb;
+                }
+                else{
+                    a+=ta;
+                    b+=tb;
+                }
+            }
+            System.out.println(a+" "+b);
+        }
+    }
+}
---
+++ 0501.java
@@ -0,0 +1,36 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+ 
+        while(true){
+            int n = stdIn.nextInt();
+            if(n==0){
+                return;
+            }
+            String[][] cv = new String[n][2];
+            for(int i=0;i<n;i++){
+                cv[i][0] = stdIn.next();
+                cv[i][1] = stdIn.next();
+            }
+            int m = stdIn.nextInt();
+            for(int i=0;i<m;i++){
+                String x = stdIn.next();
+                boolean flag = true;
+                for(int j=0;j<n;j++){
+                    if(x.equals(cv[j][0])){
+                        System.out.print(cv[j][1]);
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    System.out.print(x);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
