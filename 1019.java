446fd7286c18639978b3e0249bea4d918cf843b2
(TAT)chaN <web@lanevok.com>
2012-05-02 06:21:30 +0900

    AC

---
+++ 1019.java
@@ -0,0 +1,35 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+         
+        while(true){
+            int n = stdIn.nextInt();
+            int k = stdIn.nextInt();
+            if(n==0&&k==0){
+                return;
+            }
+            int[] have = new int[k];
+            for(int i=0;i<k;i++){
+                have[i] = stdIn.nextInt();
+            }
+            boolean f = true;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<k;j++){
+                    have[j] -= stdIn.nextInt();
+                    if(have[j]<0){
+                        f = false;
+                    }
+                }
+            }
+            if(f){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+        }
+    }
+}
