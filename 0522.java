f1d9a32428a1ed308f931ae7519ecdfcbfc2f5e9
(TAT)chaN <web@lanevok.com>
2012-05-07 21:58:35 +0900

    AC

---
+++ 0522.java
@@ -0,0 +1,24 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        while(stdIn.hasNext()){
+            char[] line = stdIn.nextLine().toCharArray();
+            int joi = 0, ioi = 0;
+            for(int i=2;i<line.length;i++){
+                if(line[i]=='I'&&line[i-1]=='O'){
+                    if(line[i-2]=='J'){
+                        joi++;
+                    }
+                    else if(line[i-2]=='I'){
+                        ioi++;
+                    }
+                }
+            }
+            System.out.println(joi);
+            System.out.println(ioi);
+        }
+    }
+}
