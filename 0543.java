d6263d4db9e160239be05e8701193e9140476c58
(TAT)chaN <web@lanevok.com>
2012-05-07 22:06:01 +0900

    AC

---
+++ 0543.java
@@ -0,0 +1,12 @@
+﻿import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner stdIn=new Scanner(System.in);
+        while(true){
+            int n=stdIn.nextInt();
+            if(n<1) return;
+            for(int i=0;i<9;i++) n-=stdIn.nextInt();
+            System.out.println(n);
+        }
+    }
+}
