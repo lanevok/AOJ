f0adf0214f3eb99354a08e7c3128051082c25bb3
(TAT)chaN <web@lanevok.com>
2012-05-10 22:35:52 +0900

    AC

---
+++ 1051.java
@@ -0,0 +1,40 @@
+﻿import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+         
+        while(true){
+            int n = stdIn.nextInt();
+            if(n==0){
+                return;
+            }
+            int[] a = new int[n];
+            for(int i=0;i<n;i++){
+                a[i] = stdIn.nextInt();
+            }
+            Arrays.sort(a);
+            for(int i=0;i<n;i++){
+                System.out.print(a[i]);
+                for(int j=i+1;j<n;j++){
+                    if(j==n-1&&a[n-2]+1==a[n-1]){
+                        System.out.print("-"+a[j]);
+                        i = j;
+                        break;
+                    }
+                    else if(a[j-1]+1!=a[j]){
+                        if(i!=j-1){
+                            System.out.print("-"+a[j-1]);
+                            i = j-1;
+                        }
+                        System.out.print(" ");
+                        break;
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
