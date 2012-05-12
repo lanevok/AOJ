d006698179e5e01451573355f429351cc085cd18
(TAT)chaN <web@lanevok.com>
2012-05-12 23:56:59 +0900

    AC

---
+++ 0167.java
@@ -0,0 +1,30 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        while(true){
+            int n = stdIn.nextInt();
+            if(n==0){
+                return;
+            }
+            int[] a = new int[n];
+            for(int i=0;i<n;i++){
+                a[i] = stdIn.nextInt();
+            }
+            int cnt = 0;
+            for(int i=0;i<n;i++){
+                for(int j=n-1;j>i;j--){
+                    if(a[j]<a[j-1]){
+                        cnt++;
+                        int tmp = a[j];
+                        a[j] = a[j-1];
+                        a[j-1] = tmp;
+                    }
+                }
+            }
+            System.out.println(cnt);
+        }
+    }
+}
---
+++ 0197.java
@@ -0,0 +1,34 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    static int cnt;
+     
+    static long gcd(long a, long b){
+        if(b==0){
+            return a;
+        }
+        else{
+            cnt++;
+            return gcd(b,a%b);
+        }
+    }
+     
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        while(true){
+            long a = stdIn.nextInt();
+            long b = stdIn.nextInt();
+            if((a|b)==0){
+                return;
+            }
+            cnt = 0;
+            if(a<b){
+                long tmp = a;
+                a = b;
+                b = tmp;
+            }
+            System.out.println(gcd(a,b)+" "+cnt);
+        }
+    }
+}
