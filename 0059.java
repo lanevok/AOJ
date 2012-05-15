a2af64cd11f1449dddf9b38651b57840754a939b
(TAT)chaN <web@lanevok.com>
2012-05-16 01:43:50 +0900

    AC

---
+++ 0059.java
@@ -0,0 +1,24 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+         
+        while(stdIn.hasNext()){
+            double xa1 = stdIn.nextDouble();
+            double ya1 = stdIn.nextDouble();
+            double xa2 = stdIn.nextDouble();
+            double ya2 = stdIn.nextDouble();
+            double xb1 = stdIn.nextDouble();
+            double yb1 = stdIn.nextDouble();
+            double xb2 = stdIn.nextDouble();
+            double yb2 = stdIn.nextDouble();
+            boolean flag = true;
+            if(xb2<xa1||xa2<xb1||ya2<yb1||yb2<ya1){
+                flag = false;
+            }
+            System.out.println(flag?"YES":"NO");
+        }
+    }
+}
---
+++ 0544.java
@@ -0,0 +1,37 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+         
+        while(true){
+            int n = stdIn.nextInt();
+            int m = stdIn.nextInt();
+            if((n|m)==0){
+                return;
+            }
+            int[] map = new int[n+1];
+            for(int i=1;i<=n;i++){
+                map[i] = stdIn.nextInt();
+            }
+            int[] dice = new int[m+1];
+            for(int i=1;i<=m;i++){
+                dice[i] = stdIn.nextInt();
+            }
+            int now = 1;
+            int i = 1;
+            for(;i<=m;i++){
+                now += dice[i];
+                if(now>=n){
+                    break;
+                }
+                now += map[now];
+                if(now>=n){
+                    break;
+                }
+            }
+            System.out.println(i);
+        }
+    }
+}
