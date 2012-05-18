b5e4076ad55e1a811e1890d4151b02d5f55ab192
(TAT)chaN <web@lanevok.com>
2012-05-19 01:03:01 +0900

    AC

---
+++ 0169.java
@@ -0,0 +1,52 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        while(true){
+            String[] sc = stdIn.nextLine().split(" ");
+            if(Integer.parseInt(sc[0])==0){
+                return;
+            }
+            int[] card = new int[sc.length];
+            for(int i=0;i<sc.length;i++){
+                card[i] = Integer.parseInt(sc[i]);
+                if(card[i]>10){
+                    card[i] = 10;
+                }
+                else if(card[i]==1){
+                    card[i] = 11;
+                }
+            }
+            int sum = 0;
+            int c11 = 0;
+            for(int i=0;i<sc.length;i++){
+                sum += card[i];
+                if(card[i]==11){
+                    c11++;
+                }
+            }
+            if(sum<=21){
+                System.out.println(sum);
+            }
+            else if(c11<1){
+                System.out.println("0");
+            }
+            else{
+                boolean flag = true;
+                for(int i=0;i<c11;i++){
+                    sum -= 10;
+                    if(sum<=21){
+                        System.out.println(sum);
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    System.out.println("0");
+                }
+            }
+        }
+    }
+}
---
+++ 2006.java
@@ -0,0 +1,38 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        final char[][] button = {
+                {},
+                {'.',',','!','?',' '},
+                {'a','b','c'},
+                {'d','e','f'},
+                {'g','h','i'},
+                {'j','k','l'},
+                {'m','n','o'},
+                {'p','q','r','s'},
+                {'t','u','v'},
+                {'w','x','y','z'}
+        };
+        int n = stdIn.nextInt();
+        for(int j=0;j<n;j++){
+            String[] key =  stdIn.next().split("0");
+            for(int i=0;i<key.length;i++){
+                if(key[i].length()<1){
+                    continue;
+                }
+                char[] tmp = key[i].toCharArray();
+                int num = tmp[tmp.length-1]-'0';
+                int times = key[i].length();
+                int pos = times%(button[num].length)-1;
+                if(pos<0){
+                    pos = button[num].length-1;
+                }
+                System.out.print(button[num][pos]);
+            }
+            System.out.println();
+        }
+    }
+}
