50a0857650c8b848f7e6fe0b5f6540bdaec8a52d
(TAT)chaN <web@lanevok.com>
2012-05-19 10:00:16 +0900

    AC

---
+++ 0516.java
@@ -0,0 +1,37 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        while(true){
+            int n = stdIn.nextInt();
+            int k = stdIn.nextInt();
+            if((n|k)==0){
+                return;
+            }
+            int[] a = new int[n];
+            for(int i=0;i<n;i++){
+                a[i] = stdIn.nextInt();
+            }
+            int max = 0;
+             
+            for(int i=0;i<k;i++){
+                max += a[i];
+            }
+            int s = max;
+            for(int i=k;i<n;i++){
+                s += a[i];
+                s -= a[i-k];
+//          TLE -------------------------
+//          for(int i=0;i<=n-k;i++){
+//              int s = 0;
+//              for(int j=i;j<i+k;j++){
+//                  s+=a[j];
+//              }
+                max = Math.max(max, s);
+            }
+            System.out.println(max);
+        }
+    }
+}
