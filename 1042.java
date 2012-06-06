7509d2e0dd5ac0cd797b252096ab0c369200cc56
(TAT)chaN <web@lanevok.com>
2012-06-06 20:13:56 +0900

    AC

---
+++ 1042.java
@@ -0,0 +1,19 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        while(true){
+            String str1 = stdIn.nextLine();
+            if(str1.equals("END OF INPUT")){
+                return;
+            }
+            String[] str2 = str1.split(" ");
+            for(int i=0;i<str2.length;i++){
+                System.out.print(str2[i].length());
+            }
+            System.out.println();
+        }
+    }
+}
