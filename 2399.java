77390058bc8525582fce52940d0e262ab4043f7f
(TAT)chaN <web@lanevok.com>
2012-06-17 23:23:59 +0900

    AC

---
+++ 2399.java
@@ -0,0 +1,62 @@
+﻿import java.io.*;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+// JAG Team Coding Source
+ 
+    public static void main(String[] args) {
+//      System.setOut(new PrintStream(new File("result.txt")));
+        Scanner stdIn = new Scanner(System.in);
+         
+        while(true){
+            int n = stdIn.nextInt();
+            if(n==0){
+                return;
+            }
+            int[][] know = new int[n+1][];
+            for(int i=1;i<=n;i++){
+                int m = stdIn.nextInt();
+                know[i] = new int[m+1];
+                for(int j=1;j<=m;j++){
+                    know[i][j] = stdIn.nextInt();
+                }
+            }
+            int k = stdIn.nextInt();
+            int[] lost = new int[k];
+            for(int i=0;i<k;i++){
+                lost[i] = stdIn.nextInt();
+            }
+            // Solve
+            int ans = -1;
+            for(int i=1;i<=n;i++){
+                int cnt = 0;
+                for(int j=0;j<k;j++){
+                    boolean flag = false;
+                    for(int m=1;m<know[i].length;m++){
+                        if(lost[j]==know[i][m]){
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if(flag){
+                        cnt++;
+                        if(cnt==k){
+                            break;
+                        }
+                    }
+                }
+                if(cnt==k){
+                    if(ans==-1){
+                        ans = i;
+                    }
+                    else{
+                        ans = -1;
+                        break;
+                    }
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+}
---
+++ 2400.java
@@ -0,0 +1,79 @@
+﻿import java.util.Scanner;
+ 
+public class Main { 
+     
+    // JAG Team Coding Source (一部改訂)
+     
+    static class Team {
+        int id;
+        int slove;
+        boolean flag[];
+        int pena[];
+        int time;
+        Team(int nid,int P){
+            this.id = nid;
+            this.slove = 0;
+            this.flag = new boolean[P+1];
+            this.pena = new int[P+1];
+        }
+    }
+     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            int t = sc.nextInt();
+            int p = sc.nextInt();
+            int r = sc.nextInt();
+            if((t|p|r)==0){
+                break;
+            }
+            Team[] team = new Team[t+1];        // チーム配列の生成
+            for(int i=0;i<=t;i++){
+                team[i] = new Team(i,p);        // チーム配列でチームクラス生成
+            }
+            for(int i=0;i<r;i++){        // Main Input
+                int tid = sc.nextInt();
+                int pid = sc.nextInt();
+                int ttime = sc.nextInt();
+                String str = sc.next();         // message
+                if(str.equals("CORRECT")){
+                    if(!(team[tid].flag[pid])){     // 1st AC
+                        team[tid].slove++;
+                        team[tid].time += team[tid].pena[pid]*1200+ttime;
+                        team[tid].flag[pid] = true;
+                    }
+                }
+                else{
+                    team[tid].pena[pid]++;
+                }
+            }
+            // Sorting (最小値選択ソート)
+            for(int i=1;i<=t;i++){
+                int max = i;
+                for(int j=i;j<=t;j++){
+                    if(team[max].slove<team[j].slove){       // より問題を解いた
+                        max=j;
+                    }
+                    else if(team[max].slove==team[j].slove){
+                        if(team[max].time>team[j].time){ // ペナルティ
+                            max=j;
+                        }
+                        else if(team[max].time==team[j].time){
+                            if(team[max].id>team[j].id){ // チームIDの若い
+                                max=j;
+                            }
+                        }
+                    }
+                }
+                // Swap
+                Team temp = team[i];
+                team[i] = team[max];
+                team[max] = temp;
+            }
+            // Output
+            for(int i=1;i<=t;i++){
+                System.out.println(team[i].id+" "+team[i].slove+" "+team[i].time);
+            }
+        }
+    }
+}
