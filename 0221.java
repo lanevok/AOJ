7177727bd840e45a199f666bc59c8cdd4367d080
(TAT)chaN <web@lanevok.com>
2012-08-31 01:10:49 +0900

    AC

---
+++ 0221.java
@@ -0,0 +1,81 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+ 
+        while(true){
+            int m = stdIn.nextInt();
+            int n = stdIn.nextInt();
+            if((m|n)==0){
+                return;
+            }
+            boolean[] a = new boolean[m+1];
+            a[0] = true;
+            int now = 0;
+            int player = m;
+            for(int i=1;i<=n;i++){
+                if(player!=1){
+                    while(true){
+                        now++;
+                        if(now==m+1){
+                            now = 1;
+                        }
+                        if(!a[now]){
+                            break;
+                        }
+                    }
+                }
+                String tmp = stdIn.next();
+                if(player==1){
+                    continue;
+                }
+                boolean judge = false;
+                if(i%3==0&&i%5==0){
+                    if(tmp.equals("FizzBuzz")){
+                        judge = true;
+                    }
+                }
+                else if(i%3==0){
+                    if(tmp.equals("Fizz")){
+                        judge = true;
+                    }
+                }
+                else if(i%5==0){
+                    if(tmp.equals("Buzz")){
+                        judge = true;
+                    }
+                }
+                else{
+                    int num = 0;
+                    try{
+                        num = Integer.parseInt(tmp);
+                    }
+                    catch(Exception e){
+                    }
+                    if(num==i){
+                        judge = true;
+                    }
+                }
+                if(!judge){
+                    a[now] = true;
+                    player--;
+                }
+            }
+            boolean flag = true;
+            for(int i=0;i<=m;i++){
+                if(!a[i]){
+                    if(flag){
+                        System.out.print(i);
+                        flag = false;
+                    }
+                    else{
+                        System.out.print(" "+i);
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ 2271.java
@@ -0,0 +1,26 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+         
+        int[] kupc = new int[4];
+        char[] s = stdIn.next().toCharArray();
+        for(int i=0;i<s.length;i++){
+            if(s[i]=='K'){
+                kupc[0]++;
+            }
+            else if(s[i]=='U'){
+                kupc[1]++;
+            }
+            else if(s[i]=='P'){
+                kupc[2]++;
+            }
+            else if(s[i]=='C'){
+                kupc[3]++;
+            }
+        }
+        System.out.println(Math.min(kupc[0], Math.min(kupc[1], Math.min(kupc[2], kupc[3]))));
+    }
+}
