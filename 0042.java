cd1abd6227a84213cb464cdadca08e3506eaa3c3
(TAT)chaN <web@lanevok.com>
2012-09-04 15:14:26 +0900

    AC

---
+++ 0042.java
@@ -0,0 +1,47 @@
+﻿import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+         
+        int Case = 1;
+        while(true){
+            int W_Bag = stdIn.nextInt();
+            if(W_Bag==0){
+                return;
+            }
+            int N_Count = stdIn.nextInt();
+            int[] value = new int[N_Count+1];
+            int[] weight = new int[N_Count+1];
+            for(int i=1;i<=N_Count;i++){
+                String[] s = stdIn.next().split(",");
+                value[i] = Integer.parseInt(s[0]);
+                weight[i] = Integer.parseInt(s[1]);
+            }
+            int[][] dp = new int[N_Count+1][W_Bag+1];
+            int maxValue = 0;
+            int minWeight = Integer.MAX_VALUE;
+            for(int i=1;i<=N_Count;i++){
+                for(int j=1;j<=W_Bag;j++){
+                    if(j-weight[i]>=0){
+                        dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-weight[i]]+value[i]);
+                        if(maxValue<dp[i][j]){
+                            maxValue = dp[i][j];
+                            minWeight = j;
+                        }
+                        else if(maxValue==dp[i][j]){
+                            minWeight = Math.min(minWeight, j);
+                        }
+                    }
+                    else{
+                        dp[i][j] = dp[i-1][j];
+                    }
+                }
+            }
+            System.out.println("Case "+Case+++":");
+            System.out.println(maxValue);
+            System.out.println(minWeight);
+        }
+    }
+}
---
+++ 0112.java
@@ -0,0 +1,28 @@
+﻿import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+         
+        while(true){
+            int n = stdIn.nextInt();
+            if(n==0){
+                return;
+            }
+            int[] w = new int[n];
+            for(int i=0;i<n;i++){
+                w[i] = stdIn.nextInt();
+            }
+            Arrays.sort(w);
+            long s = 0;
+            long t = 0;
+            for(int i=0;i<n;i++){
+                s += t;
+                t += w[i];
+            }
+            System.out.println(s);
+        }
+    }
+}
