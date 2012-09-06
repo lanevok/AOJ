10df9997f307a495d4e20ae556194cf2aa6f9163
(TAT)chaN <web@lanevok.com>
2012-09-06 16:02:15 +0900

    AC

---
+++ 0523.java
@@ -0,0 +1,78 @@
+﻿import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+         
+        while(true){
+            int n = stdIn.nextInt();
+            if(n==0){
+                return;
+            }
+            boolean[] t = new boolean[2*n+1];
+            boolean[] h = new boolean[2*n+1];
+            Arrays.fill(h, true);
+            h[0] = false;
+            for(int i=0;i<n;i++){
+                int tmp = stdIn.nextInt();
+                t[tmp] = true;
+                h[tmp] = false;
+            }
+            int tc = n;
+            int hc = n;
+            int f = 0;
+            boolean turnT = true;
+            boolean pay = false;
+            while(true){
+                pay = false;
+                if(tc==0||hc==0){
+                    break;
+                }
+                if(turnT){
+                    for(int i=1;i<=2*n;i++){
+                        if(t[i]&&i>f){
+                            t[i] = false;
+                            tc--;
+                            pay = true;
+                            f = i;
+                            break;
+                        }
+                    }
+                    if(!pay){
+                        f = 0;
+                    }
+                    turnT = false;
+                }
+                else{
+                    for(int i=1;i<=2*n;i++){
+                        if(h[i]&&i>f){
+                            h[i] = false;
+                            hc--;
+                            pay = true;
+                            f = i;
+                            break;
+                        }
+                    }
+                    if(!pay){
+                        f = 0;
+                    }
+                    turnT = true;
+                }           
+            }
+            int ts = 0;
+            int hs = 0;
+            for(int i=1;i<=2*n;i++){
+                if(t[i]){
+                    ts++;
+                }
+                if(h[i]){
+                    hs++;
+                }
+            }
+            System.out.println(hs);
+            System.out.println(ts);
+        }
+    }
+}
---
+++ 2150.java
@@ -0,0 +1,59 @@
+﻿import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    static boolean[] primes;
+ 
+    static void primeSet(final int MAX){
+        primes = new boolean[MAX+1];
+ 
+        primes[2] = true;
+        for(int i=3;i<=MAX;i+=2){
+            primes[i] = true;
+        }
+        int rt = (int) Math.sqrt(MAX);
+        for(int i=3;i<=rt;i+=2){
+            if(primes[i]){
+                for(int j=i*i;j<=MAX;j+=i){
+                    primes[j] = false;
+                }
+            }
+        }
+    }
+ 
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+         
+        while(true){
+            int n = stdIn.nextInt();
+            int p = stdIn.nextInt();
+            if(n==-1&&p==-1){
+                return;
+            }
+            primeSet(n+1000);
+            int[] u = new int[50];
+            int idx = n+1;
+            int z = 0;
+            for(int i=idx;z<u.length;i++){
+                if(primes[i]){
+                    u[z++] = i;
+                }
+            }
+            z = 0;
+            int[] v = new int[1000];
+            A:{
+                for(int i=0;i<u.length;i++){
+                    for(int j=i;j<u.length;j++){
+                        v[z++] = u[i] + u[j];
+                        if(z==1000){
+                            break A;
+                        }
+                    }
+                }
+            }
+            Arrays.sort(v);
+            System.out.println(v[p-1]);
+        }
+    }
+}
