7865697e884a77aa00d44b693dfa875b2865799d
(TAT)chaN <web@lanevok.com>
2012-09-06 19:13:28 +0900

    AC

---
+++ 0174.java
@@ -0,0 +1,17 @@
+﻿public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner stdIn = new java.util.Scanner(System.in);
+        while(true){
+            char[] s = stdIn.next().toCharArray();
+            if(s.length<2){
+                break;
+            }
+            int a=0, b=0;
+            for(int i=1;i<s.length;){
+                if(s[i++]<'B') a++; else b++;
+            }
+            if(a>b) a++; else b++;
+            System.out.println(a+" "+b);
+        }
+    }
+}
