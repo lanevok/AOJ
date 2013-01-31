112e99d0797b29bf64af0778f5afe2d17a1f4089
lanevok <web@lanevok.com>
2013-01-31 14:39:49 +0900

    AC

--- 0208.java
+++ 0208.java
@@ -1,25 +1,13 @@
-﻿import java.util.Scanner;
- 
-public class Main {
- 
-    static int[] b = {0,1,2,3,5,7,8,9};
-     
-    static void enc(int n){
-        if(n>=8){
-            enc(n/8);
-        }
-        System.out.print(b[n%8]);
-    }
-     
-    public static void main(String[] args) {
-        Scanner stdIn = new Scanner(System.in);
-        while(true){
-            int n = stdIn.nextInt();
-            if(n==0){
-                return;
-            }
-            enc(n);
-            System.out.println();
-        }
+﻿public class AOJ0208{
+    public static void main(String[] args){
+	java.util.Scanner s=new java.util.Scanner(System.in);
+	int[]m={0,1,2,3,5,7,8,9};
+	while(true){
+	    int n=s.nextInt();
+	    if(n==0)return;
+	    char[]c=Integer.toOctalString(n).toCharArray();
+	    for(n=0;n<c.length;n++)System.out.print(m[c[n]-'0']);
+	    System.out.println();
+	}
     }
 }
